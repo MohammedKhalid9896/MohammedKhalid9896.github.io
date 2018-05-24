71930f0d858682d91e59fdfe6ca22dd1238c9061
Mohammed Khalid <39526536+MohammedKhalid9896@users.noreply.github.com>
2018-05-24 17:06:42 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ welcomeCanvas.id = 'c';
 
 
 welcomeCanvas.style.position = 'absolute';
-welcomeCanvas.style.top = '500px';
+welcomeCanvas.style.top = '520px';
 welcomeCanvas.style.left = '0px';
 welcomeCanvas.width = innerWidth;
 welcomeCanvas.height = 100;
